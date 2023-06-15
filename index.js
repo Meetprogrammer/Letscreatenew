09172d0a2a5ba7e1721317d9fe676db93e7a2a04
Lets Create New <97238407+Meetprogrammer@users.noreply.github.com>
2023-06-16 03:04:57 +0530

    Update and rename index.js to 1.js

--- index.js
+++
@@ -1,9 +0,0 @@
-  $(function() {
-  new Typed('#typed', {
-		strings: ["MEET",   "CODEPEN","HTML5","JAVASCRIPT","CSS", "REACT", "RESPONSIVE", "SASS", "POSTCSS","ES2015"],
-		typeSpeed: 100,
-    backDelay: 1500,
-    loop: true,
-    callback: function(){}
-	});
-});
